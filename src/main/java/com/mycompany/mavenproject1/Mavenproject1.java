4157852c933e3979976e61ce0e1764b44c8dca0e
DarshanaRanasinghe <darshanaranasinghe100@gmail.com>
2025-01-31 16:47:15 +0530

    create repo and add first

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Darshana_Lap
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {       
+        
+    }
+}
